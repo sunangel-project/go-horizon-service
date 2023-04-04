394c99312abec3d0561dbaa3e2ecd4f9acdc092f
Fabian Weik <fabian.weik@protonmail.com>
2023-04-04 22:15:13 +0200

    send spot message to q for testing

--- src/messaging/messaging.go
+++ src/messaging/messaging.go
@@ -5,3 +5,24 @@ const GROUP = "horizon-service"
 
 const OUT_Q = "horizons"
 const ERR_Q = "error"
+
+type PartSubMessage struct {
+	Id uint `json:"id"`
+	Of uint `json:"of"`
+}
+
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
+
+type SpotSubMessage struct {
+	Dir  float64  `json:"dir"`
+	Kind string   `json:"kind"`
+	Loc  Location `json:"loc"`
+}
+
+type SpotMessage struct {
+	Part PartSubMessage `json:"part"`
+	Spot SpotSubMessage `json:"spot"`
+}
